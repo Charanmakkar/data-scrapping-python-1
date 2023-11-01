ef5e60c343a011e4bad8fbfa5ea5c518188be8fb
Charanpreet Singh <charanmakar@itechnolabs.tech>
2023-11-02 00:25:49 +0530

    new func added to excel.py

--- excel.py
+++ excel.py
@@ -1,38 +1,44 @@
 from openpyxl import Workbook, load_workbook
 
-def EXCEL1():
-    excel_file = "book1.xlsx"
+fileToReadEnteriesFrom = "book1.xlsx"
+fileToWriteData = "DATA_1.xlsx"
+
+##excel_file = "book1.xlsx"
+##workbook = load_workbook(excel_file)
+##worksheet = workbook.active
+
+def readCell(fileName, cellvalue):
+    excel_file = fileName
     workbook = load_workbook(excel_file)
     worksheet = workbook.active
-
-    maxNumberOfRows = worksheet.max_row
-    maxNumberOfColumns = worksheet.max_column
-
-
-def readCell(cellvalue):
     value = worksheet[cellvalue].value
 ##    print(value)
     return value
+
+
 def readMaxCol(fileName):
     excel_file = fileName
     workbook = load_workbook(excel_file)
     worksheet = workbook.active
     maxNumberOfColumns = worksheet.max_column
+    return maxNumberOfColumns
 
 def readMaxRow(fileName):
     excel_file = fileName
     workbook = load_workbook(excel_file)
     worksheet = workbook.active
-    maxNumberOfColumns = worksheet.max_row
+    maxNumberOfRow = worksheet.max_row
+    return maxNumberOfRow
     
-
-
-
-##readCell("A1")
-
-
-
-
+def writeToCellOfEXCEL(fileName, row, col, Value):
+    excel_file = fileName
+    workbook = load_workbook(excel_file)
+    worksheet = workbook.active
+    cell = str(col)+str(row)
+    worksheet[cell] = Value
+    workbook.save(excel_file)
+    
+    return True
 
 
 
